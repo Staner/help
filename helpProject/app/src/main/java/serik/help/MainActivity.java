930e8bb5c99fc3b99d9dd31d09311429f4e0284f
Sergei Popov <sergei.mutd@gmail.com>
2015-09-01 11:51:36 +0300

    first

--- helpProject/app/src/main/java/serik/help/MainActivity.java
+++ helpProject/app/src/main/java/serik/help/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -14,8 +15,14 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
+        final Button button = (Button) findViewById(R.id.button);
         button.setText("hallo");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setText("click");
+            }
+        });
     }
 
 
